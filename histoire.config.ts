e42f7c1f6044f280773b75c2f47306c28ea5e3cc
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-11-07 16:56:39 +0900

    chore: update deps

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'histoire'
+import { HstVue } from '@histoire/plugin-vue'
 import { HstNuxt } from '@histoire/plugin-nuxt'
 
 export default defineConfig({
-  plugins: [HstNuxt()],
+  plugins: [HstVue(), HstNuxt()],
   vite: {
     base: '/my-components/',
     server: {
