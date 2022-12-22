204cd76f7fdc47f2d3e1fe32c6b320dd5271ee96
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-12-22 10:13:45 +0900

    test(Button): add unit test

--- components/base/button/index.spec.ts
+++ components/base/button/index.spec.ts
@@ -1,13 +1,45 @@
 import { shallowMount } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
 import Button from './index.vue'
 
-let wrapper: any
+let wrapper: VueWrapper
 describe('Button.vue', () => {
-  beforeEach(() => {
-    wrapper = shallowMount(Button)
+  it('render correctly', () => {
+    wrapper = shallowMount(Button) as VueWrapper
+    expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it('is called', () => {
-    expect(wrapper.exists()).toBeTruthy()
+  describe('while loading', () => {
+    beforeEach(async () => {
+      wrapper = shallowMount(Button) as VueWrapper
+      await wrapper.setProps({ loading: true })
+    })
+
+    it('show loading message', async () => {
+      expect(wrapper.get('[aria-live="assertive"]').text()).toBe(
+        wrapper.props('loadingMsg')
+      )
+    })
+
+    it('should not be able to cick', async () => {
+      await wrapper.trigger('click')
+      expect(wrapper.emitted()).not.toHaveProperty('click')
+    })
+  })
+
+  it('emit click correctly', async () => {
+    wrapper = shallowMount(Button) as VueWrapper
+    await wrapper.trigger('click')
+    expect(wrapper.emitted().click).toHaveLength(1)
+  })
+
+  it('should not be able to cick when aria-disabled is true', async () => {
+    wrapper = shallowMount(Button, {
+      attrs: {
+        'aria-disabled': true,
+      },
+    }) as VueWrapper
+    await wrapper.trigger('click')
+    expect(wrapper.emitted()).not.toHaveProperty('click')
   })
 })
