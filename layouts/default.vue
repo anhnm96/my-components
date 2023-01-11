3497970600879a99ae65df4d0888c2a3d07f7a17
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-01-11 15:23:07 +0900

    feat: header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { clamp } from 'lodash-es'
+
+const height = ref(80)
+const opacity = ref(1)
+const bgOpacity = ref(1)
+
+const lastScrollTop = ref(0)
+function onScroll() {
+  const current = window.scrollY
+  const diff = current - lastScrollTop.value
+  const newHeight = height.value - diff
+  height.value = clamp(newHeight, 50, 80)
+
+  const newOpacity = opacity.value - diff * 0.1
+  opacity.value = clamp(newOpacity, 0, 1)
+  bgOpacity.value = clamp(newOpacity, 0.9, 1)
+  lastScrollTop.value = current <= 0 ? 0 : current
+}
+
+useEventListener('scroll', onScroll)
+</script>
 
 <template>
   <div class="bg-slate-700">
@@ -6,7 +27,13 @@
       class="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600"
     >
       <div class="z-0 flex-1 overflow-y-scroll">
-        <header class="flex h-20">
+        <header
+          class="fixed inset-x-0 z-10 flex h-20 shadow backdrop-blur-md transition-all"
+          :style="{
+            height: `${height}px`,
+            backgroundColor: `rgba(255 255 255 / ${bgOpacity})`,
+          }"
+        >
           <div
             class="mx-auto flex w-full max-w-3xl items-center justify-between px-8"
           >
@@ -22,7 +49,10 @@
                 Daily Bugle
               </span>
             </p>
-            <nav class="flex space-x-4 text-xs font-medium text-slate-400">
+            <nav
+              :style="{ opacity }"
+              class="flex space-x-4 text-xs font-medium text-slate-400 transition-opacity"
+            >
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
