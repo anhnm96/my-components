4cf4a89918fc85b371e0eae43a3c30c0c319c4a9
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-01-20 17:56:24 +0900

    feat: text gradient

---
+++ components/tailwind/Text.story.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <Story title="Text">
+    <Variant title="Gradient">
+      <p
+        style="--color-one: #42d392; --color-two: #647eff"
+        class="gradient-text text-5xl font-bold"
+      >
+        The Progressive JavaScript Framework
+      </p>
+    </Variant>
+    <Variant title="Animated Gradient">
+      <p
+        style="
+          --animated-gradient-color-one: #42d392;
+          --animated-gradient-color-two: #647eff;
+        "
+        class="animated-gradient-text text-5xl font-bold"
+      >
+        The Progressive JavaScript Framework
+      </p>
+    </Variant>
+  </Story>
+</template>
