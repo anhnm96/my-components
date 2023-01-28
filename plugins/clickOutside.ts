cd09e5854304cdad1a74f8c233fc492a11b328d3
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-01-28 22:49:56 +0900

    feat(directives): clickOutside

---
+++ plugins/clickOutside.ts
@@ -0,0 +1,23 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('clickOutside', {
+    mounted(el, binding) {
+      let targetEl = el
+      if (binding.arg === 'parent') targetEl = el.parentElement
+      targetEl.__ClickOutsideHandler__ = (event: Event) => {
+        // check if event's target is the el or contained by el
+        if (!(targetEl === event.target || targetEl.contains(event.target))) {
+          binding.value(event)
+        }
+      }
+      document.body.addEventListener('click', targetEl.__ClickOutsideHandler__)
+    },
+    beforeUnmount(el, binding) {
+      let targetEl = el
+      if (binding.arg === 'parent') targetEl = el.parentElement
+      document.body.removeEventListener(
+        'click',
+        targetEl.__ClickOutsideHandler__
+      )
+    },
+  })
+})
