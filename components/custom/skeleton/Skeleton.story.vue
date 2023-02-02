fdb86fab1ab37a2ed27af1e4b591840ad998b499
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-02-02 11:20:22 +0900

    feat(components): add Skeleton

---
+++ components/custom/skeleton/Skeleton.story.vue
@@ -0,0 +1,28 @@
+<template>
+  <Story title="Skeleton">
+    <div class="max-w-sm rounded border border-gray-200 p-4 md:p-6">
+      <Skeleton
+        class="mb-4 flex h-48 items-center justify-center text-gray-400"
+      >
+        Image
+      </Skeleton>
+      <Skeleton auto shimmer-color="#46CE93" class="mb-4 h-2.5" />
+      <Skeleton auto shimmer-color="#46CE93" class="mb-2.5 h-2" />
+      <Skeleton auto shimmer-color="#46CE93" class="mb-2.5 h-2" />
+      <Skeleton auto shimmer-color="#46CE93" class="h-2" />
+      <div class="mt-4 flex items-center space-x-3">
+        <Skeleton
+          shimmer-color="#46CE93"
+          class="flex h-14 w-14 items-center justify-center text-gray-400"
+          css-class="rounded-full"
+        >
+          Icon
+        </Skeleton>
+        <div>
+          <Skeleton class="mb-2 h-2.5 w-32" />
+          <Skeleton class="h-2 w-48" />
+        </div>
+      </div>
+    </div>
+  </Story>
+</template>
---
+++ components/custom/skeleton/Skeleton.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { hexToRgb, isHexColor } from '@/helpers/color'
+
+interface RandomRange {
+  min: number
+  max: number
+}
+const props = withDefaults(
+  defineProps<{
+    bgClass?: string
+    cssClass?: string
+    shimmerColor?: string
+    auto?: boolean
+    range?: RandomRange
+  }>(),
+  {
+    bgClass: 'bg-gray-300',
+    cssClass: 'rounded',
+    shimmerColor: '#ffffff',
+    auto: false,
+    range: () => ({
+      min: 20,
+      max: 100,
+    }),
+  }
+)
+
+const shimmerStyle = computed(() => {
+  const rgb = isHexColor(props.shimmerColor)
+    ? hexToRgb(props.shimmerColor)
+    : props.shimmerColor
+
+  return {
+    backgroundImage: `linear-gradient(90deg, rgba(${rgb}, 0) 0%, rgba(${rgb}, 0.2) 20%, rgba(${rgb}, 0.5) 60%, rgba(${rgb}, 0))`,
+  }
+})
+
+const width = computed(() => {
+  return `${Math.floor(
+    Math.random() * (props.range.max - props.range.min) + props.range.min
+  )}%`
+})
+</script>
+
+<template>
+  <div
+    aria-hidden="true"
+    class="relative overflow-hidden"
+    :class="[bgClass, cssClass]"
+    :style="{ width: auto === true ? width : '' }"
+  >
+    <div class="shimmer absolute inset-0" :style="shimmerStyle"></div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.shimmer {
+  transform: translateX(-100%);
+  animation: shimmer 1.4s infinite;
+}
+
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
