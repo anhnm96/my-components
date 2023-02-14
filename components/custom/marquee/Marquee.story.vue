46fa3e01bdc1814b97bf66681bdaf81918e6532d
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-02-14 10:50:09 +0900

    feat(stories): add Marquee demo

--- components/custom/marquee/Marquee.story.vue
+++ components/custom/marquee/Marquee.story.vue
@@ -9,26 +9,65 @@ const marqueeList = ref([
 </script>
 
 <template>
-  <Story title="Marquee">
-    <Marquee
-      v-slot="{ item }"
-      :list="marqueeList"
-      style="--gap: 0rem; --duration: 15s"
-    >
-      <div
-        class="m-auto w-full py-2 text-center text-white"
-        :class="[`bg-${item.color}-500`]"
+  <Story title="Marquee" :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="no gap, custom animation">
+      <Marquee
+        v-slot="{ item }"
+        :list="marqueeList"
+        style="--gap: 0rem; --duration: 25s"
       >
-        {{ item.text }}
-      </div>
-    </Marquee>
-    <Marquee v-slot="{ item }" :list="marqueeList" class="mt-4">
-      <div
-        class="w-full rounded py-2 text-center text-white"
-        :class="[`bg-${item.color}-500`]"
+        <div
+          class="m-auto w-full py-2 text-center text-white"
+          :class="[`bg-${item.color}-500`]"
+        >
+          {{ item.text }}
+        </div>
+      </Marquee>
+    </Variant>
+    <Variant title="normal">
+      <Marquee v-slot="{ item }" :list="marqueeList" class="mt-4">
+        <div
+          class="w-full rounded py-2 text-center text-white"
+          :class="[`bg-${item.color}-500`]"
+        >
+          {{ item.text }}
+        </div>
+      </Marquee>
+    </Variant>
+    <Variant title="reverse">
+      <Marquee v-slot="{ item }" :list="marqueeList" class="reverse mt-4">
+        <div
+          class="w-full rounded py-2 text-center text-white"
+          :class="[`bg-${item.color}-500`]"
+        >
+          {{ item.text }}
+        </div>
+      </Marquee>
+    </Variant>
+    <Variant title="normal, rtl">
+      <Marquee v-slot="{ item }" :list="marqueeList" class="mt-4" dir="rtl">
+        <div
+          class="w-full rounded py-2 text-center text-white"
+          :class="[`bg-${item.color}-500`]"
+        >
+          {{ item.text }}
+        </div>
+      </Marquee>
+    </Variant>
+    <Variant title="reverse, rtl">
+      <Marquee
+        v-slot="{ item }"
+        :list="marqueeList"
+        class="reverse mt-4"
+        dir="rtl"
       >
-        {{ item.text }}
-      </div>
-    </Marquee>
+        <div
+          class="w-full rounded py-2 text-center text-white"
+          :class="[`bg-${item.color}-500`]"
+        >
+          {{ item.text }}
+        </div>
+      </Marquee>
+    </Variant>
   </Story>
 </template>
