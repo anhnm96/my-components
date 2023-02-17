465e8fe9925d3e1c39e3659cb9eb2d22d44169d8
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-02-17 17:18:15 +0900

    Updates

---
+++ assets/index-ec857367.js
@@ -0,0 +1,24 @@
+import { aH as ref, aM as watch, aL as computed } from "./vendor-d1442346.js";
+function useInternalValue(props, key = "modelValue", emit, options = {}) {
+  const { eventName } = options;
+  const _modelValue = ref(props[key]);
+  watch(
+    () => props[key],
+    (val) => {
+      _modelValue.value = val;
+    }
+  );
+  const event = eventName || `update:${key.toString()}`;
+  return computed({
+    set(value) {
+      _modelValue.value = value;
+      emit == null ? void 0 : emit(event, value);
+    },
+    get() {
+      return _modelValue.value;
+    }
+  });
+}
+export {
+  useInternalValue as u
+};
