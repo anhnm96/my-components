16a9224d105d2beef46e78e100fdb1a24866430c
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-02-23 16:42:14 +0900

    feat(stories): add gradient shadow button

--- components/tailwind/Button.story.vue
+++ components/tailwind/Button.story.vue
@@ -1,5 +1,5 @@
 <template>
-  <Story title="Button" :layout="{ type: 'grid' }">
+  <Story title="Button" :layout="{ type: 'grid', width: '300px' }">
     <Variant title="Pulse">
       <div class="p-4">
         <button
@@ -16,6 +16,15 @@
         <span class="absolute inset-[2px] -z-[1] rounded bg-black"></span>
         <span class="text-neutral-400">Button</span>
       </button>
+    </Variant>
+    <Variant title="Gradient Shadow">
+      <div class="p-14">
+        <BaseButton
+          class="border-gradient before:transition-animation relative isolate rounded-lg border-2 border-solid border-transparent text-base before:absolute before:-inset-4 before:z-[-1] before:rounded-lg before:bg-gradient-to-r before:from-[#6658fe] before:via-[#9f6afe] before:to-[#b79dfe] before:opacity-0 before:blur-sm hover:before:opacity-75 hover:before:blur-xl"
+        >
+          Hover me
+        </BaseButton>
+      </div>
     </Variant>
   </Story>
 </template>
@@ -51,5 +60,16 @@
   100% {
     transform: translateY(-25%) scale(2) rotate(360deg);
   }
+}
+
+.border-gradient {
+  background: linear-gradient(90deg, #18181b, #18181b),
+    linear-gradient(to bottom right, #6171fe, #9f6afe, #b79dfe);
+  background-clip: padding-box, border-box;
+  background-origin: padding-box, border-box;
+}
+
+.before\:transition-animation:before {
+  transition: opacity 1s ease-in-out, filter 1s ease-in-out;
 }
 </style>
