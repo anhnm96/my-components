0dd349103a582657a4a158028ca1a2207b4b79d2
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-03-31 16:00:38 +0900

    feat(VMotion): abstract functions out

--- components/custom/carousel/Gallery.vue
+++ components/custom/carousel/Gallery.vue
@@ -5,7 +5,6 @@ import img3 from '/images/3.jpeg'
 import img4 from '/images/4.jpeg'
 import img5 from '/images/5.jpeg'
 import img6 from '/images/6.jpeg'
-const index = ref(0)
 const images = [
   img1,
   img2,
@@ -33,32 +32,16 @@ const images = [
   `${img6}?3`,
 ]
 
-const transition = { duration: 0.7, easing: [0.4, 0, 0.2, 1] }
-// [0.32, 0.72, 0, 1
 const collapsedAspectRatio = 1 / 3
 const fullAspectRatio = 3 / 2
 const margin = 12
 const gap = 2
 
-const variants3 = {
+const variants = {
   active: {
     aspectRatio: fullAspectRatio,
     marginLeft: margin,
     marginRight: margin,
-    opacity: 1,
-  },
-  inactive: {
-    aspectRatio: collapsedAspectRatio,
-    marginLeft: 0,
-    marginRight: 0,
-    opacity: 0.5,
-  },
-}
-const variants = {
-  active: {
-    aspectRatio: fullAspectRatio,
-    marginLeft: `${margin}%`,
-    marginRight: `${margin}%`,
     opacity: 1,
   },
   inactive: {
@@ -142,9 +125,7 @@ const variants = {
             <VMotion
               v-for="(image, i) in images"
               :key="image"
-              :animate="
-                i === activeIndex ? variants3.active : variants3.inactive
-              "
+              :animate="i === activeIndex ? variants.active : variants.inactive"
               class="shrink-0 hover:!opacity-100"
               as="button"
               @click="scrollTo(i)"
@@ -157,118 +138,6 @@ const variants = {
           </div>
         </div>
       </Carousel>
-      <div v-if="false" class="relative overflow-hidden">
-        <div
-          :style="{ transform: `translateX(-${index * 100}%)` }"
-          class="linear flex transition-transform duration-[400ms]"
-        >
-          <img
-            v-for="image in images"
-            :key="image"
-            :src="image"
-            class="aspect-[3/2] object-cover"
-          />
-        </div>
-        <Transition name="custom-fade">
-          <button
-            v-if="index > 0"
-            class="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white opacity-70 transition-opacity duration-300 hover:opacity-100"
-            @click="index--"
-          >
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 19.5L8.25 12l7.5-7.5"
-              />
-            </svg>
-          </button>
-        </Transition>
-        <!-- <Presence :initial="false">
-          <Motion
-            v-if="index > 0"
-            tag="button"
-            :initial="{ opacity: 0 }"
-            :animate="{ opacity: 0.7 }"
-            :exit="{ opacity: 0, pointerEvents: 'none' }"
-            :transition="transition"
-            class="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-            @click="index--"
-          >
-          </Motion>
-        </Presence> -->
-        <Transition name="custom-fade">
-          <button
-            v-if="index + 1 < images.length"
-            class="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white opacity-70 transition-opacity duration-300 hover:opacity-100"
-            @click="index++"
-          >
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </svg>
-          </button>
-        </Transition>
-        <!-- <Presence :initial="false">
-          <Motion
-            v-if="index + 1 < images.length"
-            tag="button"
-            :initial="{ opacity: 0 }"
-            :animate="{ opacity: 0.7 }"
-            :exit="{ opacity: 0, pointerEvents: 'none' }"
-            :transition="transition"
-            class="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-            @click="index++"
-          >
-          </Motion>
-        </Presence> -->
-      </div>
-
-      <div
-        v-if="false"
-        class="absolute inset-x-0 bottom-6 flex h-14 justify-center overflow-hidden"
-      >
-        <div
-          :style="{
-            aspectRatio: fullAspectRatio,
-            gap: `${gap}%`,
-            transform: `translateX(-${
-              index * 100 * (collapsedAspectRatio / fullAspectRatio) +
-              margin +
-              index * gap
-            }%) translateZ(0px)`,
-          }"
-          class="flex transition-all"
-        >
-          <VMotion
-            v-for="(image, i) in images"
-            :key="image"
-            :animate="i === index ? variants3.active : variants3.inactive"
-            class="shrink-0 hover:!opacity-100"
-            as="button"
-            @click="index = i"
-          >
-            <img :src="image" class="h-full object-cover" />
-          </VMotion>
-        </div>
-      </div>
     </div>
   </div>
 </template>
--- components/custom/motion/VMotion.vue
+++ components/custom/motion/VMotion.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 type DynamicStyle = {
-  [K in keyof CSSStyleDeclaration]: { [K2 in K]: number }
+  [K in keyof CSSStyleDeclaration]: { [K2 in K]: string | number }
 }[keyof CSSStyleDeclaration]
 const props = withDefaults(
   defineProps<{
@@ -13,14 +13,18 @@ const props = withDefaults(
 )
 
 const stateArr = ref<number[]>([])
+function objectToVector(obj: Record<string, string | number>): number[] {
+  const values = Object.values(obj)
+  const vector = values.map((value) =>
+    typeof value === 'string' ? parseFloat(value) : value
+  )
+  return vector
+}
 watch(
   () => props.animate,
   (val) => {
     if (!val) return
-    stateArr.value = []
-    for (const [, value] of Object.entries(val)) {
-      stateArr.value.push(value)
-    }
+    stateArr.value = objectToVector(val)
   },
   { immediate: true }
 )
@@ -30,15 +34,34 @@ const transition = useTransition(stateArr, {
   transition: [0.4, 0, 0.2, 1],
 })
 
-const style = computed(() => {
-  const res: DynamicStyle = {}
-  let i = 0
-  for (const key in props.animate) {
-    res[key as keyof DynamicStyle] = transition.value[i]
-    if (key.startsWith('margin')) res[key] += '%'
-    i++
+function getSuffix(str: string): string {
+  const match = str.match(/^(.*?)([^\d]*)$/)
+  return match ? match[2] : ''
+}
+
+function vectorToObject(
+  vector: number[],
+  obj: Record<string, string | number>
+): Record<string, string | number> {
+  const keys = Object.keys(obj)
+  const newObj: DynamicStyle = {}
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i] as keyof DynamicStyle
+    let value: string | number = vector[i]
+    if (typeof obj[key] === 'string') {
+      const suffix = getSuffix(obj[key] as string)
+      if (suffix) {
+        value = `${value}${suffix}`
+      }
+    }
+    newObj[key] = value
   }
-  return res
+  return newObj
+}
+
+const style = computed(() => {
+  return vectorToObject(transition.value, props.animate)
 })
 </script>
 
