246ba9d9dea07e0f16e67b1ed0f809f9728ae6bf
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-04-21 19:47:39 +0900

    feat(stories): border hover button

---
+++ components/tailwind/buttons/BorderHover.vue
@@ -0,0 +1,44 @@
+<template>
+  <button class="btn">
+    Hover Me
+    <svg class="border-container">
+      <rect pathLength="100" class="border-line"></rect>
+    </svg>
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  font-weight: 700;
+  position: relative;
+  border: 0;
+  background-color: #ffdd99;
+  color: #5d4f0f;
+  transition: background 200ms ease-in-out;
+}
+
+.border-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  inset: 0;
+}
+
+.border-line {
+  width: 100%;
+  height: 100%;
+  fill: transparent;
+  stroke: #5d4f0f;
+  stroke-width: 5px;
+  stroke-dasharray: 0 100;
+  transition: stroke-dasharray 750ms ease-in-out;
+}
+
+.btn:hover {
+  background-color: transparent;
+}
+
+.btn:hover .border-line {
+  stroke-dasharray: 100 0;
+}
+</style>
