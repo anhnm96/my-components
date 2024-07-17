ec5205e2bdd740930362de4e9782a08ac4d9da1a
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-07-17 19:39:36 +0700

    feat: demo line chart

--- pages/charts.vue
+++ pages/charts.vue
@@ -6,6 +6,14 @@ definePageMeta({
 
 <template>
   <div class="flex">
-    <BarChart />
+    <!-- <BarChart /> -->
+    <div class="w-[70%]">
+      <h2>Weekly average temperature</h2>
+      <LineChart />
+    </div>
+    <div class="w-[30%]">
+      <h2>Days with precipitation</h2>
+      <Arc />
+    </div>
   </div>
 </template>
